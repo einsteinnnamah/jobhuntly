8d231350944c26c888dd07e976b3734c504d8a81
Einsteinnamah12 <67486904+Einsteinnamah12@users.noreply.github.com>
2022-04-05 20:29:31 +0100

    einstein

---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../img/Logo.png'
+import banner_img from '../img/BG.png'
+function Login() {
+return (
+  <div className='container p-10'>
+    
+    <header className=''>
+      <img src={logo}></img>
+    </header>
+    
+    <div className='banner'>
+      <div className='banner-img'>
+        <img src={banner_img}></img>
+      </div>
+      <div className='banner-text'>
+
+      </div>
+    </div>
+     
+  </div>
+  )
+}
+
+export default Login
